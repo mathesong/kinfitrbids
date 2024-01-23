89eade656729e9851c2de17145d85effe8502b1b
Martin Norgaard <martin.noergaard@nru.dk>
2024-01-23 15:01:48 +0100

    ENH: update subsetting to accommodate new tags

--- R/subsetting.R
+++ R/subsetting.R
@@ -5,33 +5,29 @@ parse_config_subsets <- function(config) {
   sub <- stringr::str_remove_all(config$Subsets$sub, " ")
   ses <- stringr::str_remove_all(config$Subsets$ses, " ")
   rec <- stringr::str_remove_all(config$Subsets$rec, " ")
-  task <- stringr::str_remove_all(config$Subsets$task, " ")
-  run <- stringr::str_remove_all(config$Subsets$run, " ")
   TracerName <- stringr::str_remove_all(config$Subsets$TracerName, " ")
-  ModeOfAdministration <- stringr::str_remove_all(config$Subsets$ModeOfAdministration, " ")
-  InstitutionName <- stringr::str_remove_all(config$Subsets$InstitutionName, " ")
-  PharmaceuticalName <- stringr::str_remove_all(config$Subsets$PharmaceuticalName, " ")
+  seg <- stringr::str_remove_all(config$Region$seg, " ")
+  pvc <- stringr::str_remove_all(config$Region$pvc, " ")
+  deriv <- stringr::str_remove_all(config$Region$deriv, " ")
+  
 
   sub <-  str_split(sub, ";")[[1]]
   ses <-  str_split(ses, ";")[[1]]
   rec <-  str_split(rec, ";")[[1]]
-  task <- str_split(task, ";")[[1]]
-  run <-  str_split(run, ";")[[1]]
   TracerName <- str_split(TracerName, ";")[[1]]
-  ModeOfAdministration <- str_split(ModeOfAdministration, ";")[[1]]
-  InstitutionName <- str_split(InstitutionName, ";")[[1]]
-  PharmaceuticalName <- str_split(PharmaceuticalName, ";")[[1]]
+  seg <- str_split(seg, ";")[[1]]
+  pvc <- str_split(pvc, ";")[[1]]
+  deriv <- str_split(deriv, ";")[[1]]
+
 
   all_tibble <- tibble::as_tibble(
     expand.grid(sub = sub,
               ses = ses,
               rec = rec,
-              task = task,
-              run = run,
               TracerName = TracerName,
-              ModeOfAdministration = ModeOfAdministration,
-              InstitutionName = InstitutionName,
-              PharmaceuticalName = PharmaceuticalName)) %>%
+              seg = seg,
+              pvc = pvc,
+              deriv = deriv)) %>%
     dplyr::mutate(across(where(is.factor), as.character))
 
   clean_tibble <- all_tibble[,!apply(all_tibble, 2, function(x) (all(x=="")))]
